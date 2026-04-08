25b6706954d5ff96aa90ff13880a1e304c98f330
Christophe <christophegranger36@gmail.com>
2026-04-08 11:34:33 -0400

    css html et compte a rebours

---
+++ course.js
@@ -0,0 +1,22 @@
+function demarrerCourse() {
+  let message = document.getElementById("message");
+  message.style.display = "block";
+
+  let textes = ["À vos marque,", "prêt?", "PARTEZ"];
+  let index = 0;
+
+  let interval = setInterval(() => {
+    message.textContent = textes[index];
+
+    index++;
+
+    if (index === textes.length) {
+      clearInterval(interval);
+
+      setTimeout(() => {
+        message.style.display = "none";
+        lancerCourse(); // 👉 démarre après le compte à rebours
+      }, 1000);
+    }
+  }, 1000);
+}
