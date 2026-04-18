4a95a32d6ff253d01f06985de158db407b168358
Christophe <christophegranger36@gmail.com>
2026-04-18 15:18:40 -0400

    chrono en toString, amélioration de l'arbo du javascript, et aussi  des fonctions. ajout de la fonction arreter qui arrete le chrono des deux voiture

--- course.js
+++ course.js
@@ -1,170 +1,95 @@
 let voitureMauve = document.getElementById("mauve");
-
 let voitureRouge = document.getElementById("rouge");
-
-let translationX1;
-
-let translationX2;
-
+let maxTranslationXMauve;
+let maxTranslationXRouge;
 let containerWidth = document.getElementById("courseContainer").offsetWidth;
-
 let voitureMauveWidth = document.getElementById("mauve").offsetWidth;
-
 let voitureRougeWidth = document.getElementById("rouge").offsetWidth;
+let message = document.getElementById("message");
+// Variables intervalles
 
-
+let intervalChrono;
+let intervalCourse;
 
 // Compte a rebour:départ
-
 function compteARebour() {
-
   let textes = ["À vos marque,", "prêt?", "PARTEZ"];
-
   let index = 0;
-
-
-
-  let interval = setInterval(() => {
-
+  message.textContent = textes[index];
+  message.style.display = "flex";
+  let intervalMessage = setInterval(() => {
     message.textContent = textes[index];
-
-
-
     index++;
-
-
-
     if (index === textes.length) {
-
-      clearInterval(interval);
-
-
-
+      clearInterval(intervalMessage);
       setTimeout(() => {
-
         message.style.display = "none";
-
-        lancerCourse();
-
+        chronometre();
       }, 1000);
-
     }
-
   }, 1000);
-
 }
-
-
-
-// translation randomiser
-
-function aleatoireTranslationX() {}
-
-
-
-// Déplacement des voitures
-
-function deplacerVoiture() {}
-
-
-
-// Départ des voitures
-
-function demarrerCourse() {
-
-  // voitureMauve.animate([{}]);
-
-  let message = document.getElementById("message");
-
-  message.style.display = "block";
-
-  compteARebour(message);
-
-  setTimeout(() => {
-
-    chronometre();
-
-  }, 4000); // attendre 4 seconde avant de démarer le chrono
-
-
-
-  chronometre();
-
-}
-
-
-
-// Arreter la voiture
-
-function arreterCourse() {}
 
 // chrono
-
 let debutMauve = 0;
-
 let debutRouge = 0;
-
 let chronometreMauve = document.getElementById("chrono1");
-
 let chronometreRouge = document.getElementById("chrono2");
 
 function chronometre() {
-
   debutMauve = Date.now();
-
   debutRouge = Date.now();
-
-  setInterval(() => {
-
+  intervalChrono = setInterval(() => {
     let tempsActuelle = Date.now();
-
     let tempsMauve = tempsActuelle - debutMauve;
-
     let tempsRouge = tempsActuelle - debutRouge;
-
     let minMauve = Math.floor(tempsMauve / 60000);
-
     let secMauve = Math.floor((tempsMauve % 60000) / 1000);
-
     let msMauve = tempsMauve % 1000;
 
-
-
     let minRouge = Math.floor(tempsRouge / 60000);
-
     let secRouge = Math.floor((tempsRouge % 60000) / 1000);
-
     let msRouge = tempsRouge % 1000;
-
     chronometreMauve.textContent =
-
-      String(minMauve).padStart(2, "0") +
-
+      minMauve.toString().padStart(2, "0") +
       ":" +
-
-      String(secMauve).padStart(2, "0") +
-
+      secMauve.toString().padStart(2, "0") +
       ":" +
-
-      String(msMauve).padStart(3, "0");
-
-
+      msMauve.toString().padStart(3, "0");
 
     chronometreRouge.textContent =
-
-      String(minRouge).padStart(2, "0") +
-
+      minRouge.toString().padStart(2, "0") +
       ":" +
-
-      String(secRouge).padStart(2, "0") +
-
+      secRouge.toString().padStart(2, "0") +
       ":" +
-
-      String(msRouge).padStart(3, "0");
-
+      msRouge.toString().padStart(3, "0");
   }, 10);
-
+  // fin chrono
 }
 
-// fin chrono
+// Départ des voitures
+function demarrerCourse() {
+  compteARebour();
+}
 
+// Déplacement des voitures
+function deplacerVoiture() {
+  // translation randomiser
+  function aleatoireTranslationX() {
+    let maxTranslationXMauve = containerWidth - voitureMauveWidth;
+    let maxTranslationXRouge = containerWidth - voitureRougeWidth;
+
+    //Distance aléatoire pour les deux voitures
+    let randomXRouge = Math.random() * maxTranslationXMauve;
+    let randomXMauve = Math.random() * maxTranslationXRouge;
+    Math.floor(randomX); // tu doit nommer ton set interval comme j'ai fait pour le chrono ex intervalTrasnlation = set intervalles
+  }
+}
+
+// Arreter la voiture
+function arreterCourse() {
+  clearInterval(intervalChrono);
+  clearInterval(translationVoiture); // NOAH nomme ton setintervalle comme ça "translationVoiture" ou change celui la et nomme le d'une autre façons c comme tu veux
+}
+
+//  FAIRE LANCER COURSE AVEC A L'INTÉRIEUR CHRONO ET TOUT LE RESTE POUR
